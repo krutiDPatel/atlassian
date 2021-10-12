0dbfea9be29fad10e12337bc6ced8515f1e14304
Kruti Patel <Kruti@192-168-1-102.tpgi.com.au>
2021-10-12 19:24:05 +1100

    All files commit

---
+++ data.js
@@ -0,0 +1,27 @@
+// Arrow characters to use: ▼ ▶ •
+
+
+
+var num = 100;
+console.log(`"Value of Number is ${num}"`);
+//fetchData(backendData);
+
+const barkCount = 3
+
+const backtick = `Byron barks ${barkCount} times`;
+
+console.log("backtick " + backtick);
+
+var stu1 = "Robyn";
+var stu2 = "Gary";
+var stu3 = "Devs";
+var marks = 100;
+
+if(stu2= "Gary" && (stu1 == "Robyn" || stu3 == "Devs")){
+    if(marks == 100){
+        alert("Pass");
+    }    
+}
+else {
+    alert("No");
+}
